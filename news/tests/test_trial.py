a26b923fd786bd1b1ade614d3d40f476eede5718
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-26 17:37:07 +0300

    add test for creation

---
+++ news/tests/test_trial.py
@@ -0,0 +1,27 @@
+# news/tests/test_trial.py
+from django.test import TestCase
+
+# Импортируем модель, чтобы работать с ней в тестах.
+from news.models import News
+
+
+# Создаём тестовый класс с произвольным названием, наследуем его от TestCase.
+class TestNews(TestCase):
+
+    # В методе класса setUpTestData создаём тестовые объекты.
+    # Оборачиваем метод соответствующим декоратором.
+    @classmethod
+    def setUpTestData(cls):
+        # Стандартным методом Django ORM create() создаём объект класса.
+        # Присваиваем объект атрибуту класса: назовём его news.
+        cls.news = News.objects.create(
+            title='Заголовок новости',
+            text='Тестовый текст',
+        )
+
+    # Проверим, что объект действительно был создан.
+    def test_successful_creation(self):
+        # При помощи обычного ORM-метода посчитаем количество записей в базе.
+        news_count = News.objects.count()
+        # Сравним полученное число с единицей.
+        self.assertEqual(news_count, 1)
