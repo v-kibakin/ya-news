2566c13398462baececec6b78c1c40d10c1fde53
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-26 17:38:59 +0300

    add post title check

--- news/tests/test_trial.py
+++ news/tests/test_trial.py
@@ -5,23 +5,24 @@ from django.test import TestCase
 from news.models import News
 
 
-# Создаём тестовый класс с произвольным названием, наследуем его от TestCase.
 class TestNews(TestCase):
+    # Все нужные переменные сохраняем в атрибуты класса.
+    TITLE = 'Заголовок новости'
+    TEXT = 'Тестовый текст'
 
-    # В методе класса setUpTestData создаём тестовые объекты.
-    # Оборачиваем метод соответствующим декоратором.
     @classmethod
     def setUpTestData(cls):
-        # Стандартным методом Django ORM create() создаём объект класса.
-        # Присваиваем объект атрибуту класса: назовём его news.
         cls.news = News.objects.create(
-            title='Заголовок новости',
-            text='Тестовый текст',
+            # При создании объекта обращаемся к константам класса через cls.
+            title=cls.TITLE,
+            text=cls.TEXT,
         )
 
-    # Проверим, что объект действительно был создан.
     def test_successful_creation(self):
-        # При помощи обычного ORM-метода посчитаем количество записей в базе.
         news_count = News.objects.count()
-        # Сравним полученное число с единицей.
         self.assertEqual(news_count, 1)
+
+    def test_title(self):
+        # Чтобы проверить равенство с константой -
+        # обращаемся к ней через self, а не через cls:
+        self.assertEqual(self.news.title, self.TITLE)
