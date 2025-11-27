4d147376c948fc3ea7b0ce6e0a3edaf5ff6a1f02
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-27 11:26:31 +0300

    add tests for login and singup

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -14,25 +14,14 @@ class TestRoutes(TestCase):
         cls.news = News.objects.create(title='Заголовок', text='Текст')
 
     def test_pages_availability(self):
-        # Создаём набор тестовых данных - кортеж кортежей.
-        # Каждый вложенный кортеж содержит два элемента:
-        # имя пути и позиционные аргументы для функции reverse().
         urls = (
-            # Путь для главной страницы не принимает
-            # никаких позиционных аргументов,
-            # поэтому вторым параметром ставим None.
             ('news:home', None),
-            # Путь для страницы новости
-            # принимает в качестве позиционного аргумента
-            # id записи; передаём его в кортеже.
-            ('news:detail', (self.news.id,))
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:signup', None),
         )
-        # Итерируемся по внешнему кортежу
-        # и распаковываем содержимое вложенных кортежей:
         for name, args in urls:
             with self.subTest(name=name):
-                # Передаём имя и позиционный аргумент в reverse()
-                # и получаем адрес страницы для GET-запроса:
                 url = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
