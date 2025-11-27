416e9b3ade089986fd2c10de10fb7b602d59e74a
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-27 11:38:56 +0300

    return tests for pages

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -28,6 +28,19 @@ class TestRoutes(TestCase):
             text='Текст комментария'
         )
 
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
     def test_availability_for_comment_edit_and_delete(self):
         users_statuses = (
             (self.author, HTTPStatus.OK),
