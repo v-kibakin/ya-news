d4db919a168a3171199fc42cbe72e8341b2df4b9
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-27 12:49:14 +0300

    add test for number of news on page

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -14,3 +14,14 @@ class TestHomePage(TestCase):
             news = News(title=f'Новость {index}', text='Просто текст.')
             all_news.append(news)
         News.objects.bulk_create(all_news)
+
+    def test_news_count(self):
+        # Загружаем главную страницу.
+        response = self.client.get(self.HOME_URL)
+        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
+        # Получаем список объектов из словаря контекста.
+        object_list = response.context['object_list']
+        # Определяем количество записей в списке.
+        news_count = object_list.count()
+        # Проверяем, что на странице именно 10 новостей.
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
