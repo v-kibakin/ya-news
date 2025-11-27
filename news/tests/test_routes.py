7fe332df97c966c5a86786e6e14e999077556d02
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-27 11:35:51 +0300

    add test for redirect

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -43,3 +43,22 @@ class TestRoutes(TestCase):
                     url = reverse(name, args=(self.comment.id,))
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
+
+    def test_redirect_for_anonymous_client(self):
+        # Сохраняем адрес страницы логина:
+        login_url = reverse('users:login')
+        # В цикле перебираем имена страниц, с которых ожидаем редирект:
+        for name in ('news:edit', 'news:delete'):
+            with self.subTest(name=name):
+                # Получаем адрес страницы редактирования
+                # или удаления комментария:
+                url = reverse(name, args=(self.comment.id,))
+                # Получаем ожидаемый адрес страницы логина,
+                # на который будет перенаправлен пользователь.
+                # Учитываем, что в адресе будет параметр next,
+                # в котором передаётся
+                # адрес страницы, с которой пользователь был переадресован.
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                # Проверяем, что редирект приведёт именно на указанную ссылку.
+                self.assertRedirects(response, redirect_url)
