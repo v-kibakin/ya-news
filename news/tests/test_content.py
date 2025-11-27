c34bd2ecd88626a9fa59d9f381745748b1262740
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-27 13:19:53 +0300

    add tests for forms

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
+from news.forms import CommentForm
 from news.models import Comment, News
 
 from datetime import datetime, timedelta
@@ -92,3 +93,15 @@ class TestDetailPage(TestCase):
         sorted_timestamps = sorted(all_timestamps)
         # Проверяем, что временные метки отсортированы правильно.
         self.assertEqual(all_timestamps, sorted_timestamps)
+
+    def test_anonymous_client_has_no_form(self):
+        response = self.client.get(self.detail_url)
+        self.assertNotIn('form', response.context)
+
+    def test_authorized_client_has_form(self):
+        # Авторизуем клиент при помощи ранее созданного пользователя.
+        self.client.force_login(self.author)
+        response = self.client.get(self.detail_url)
+        self.assertIn('form', response.context)
+        # Проверим, что объект формы соответствует нужному классу формы.
+        self.assertIsInstance(response.context['form'], CommentForm)
